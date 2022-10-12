a93e5ac19c2f0b8665dd398487428f12c7b37c65
sankalpSRM <113873509+sankalpSRM@users.noreply.github.com>
2022-10-12 10:07:35 +0530

    Update and rename Size of Variables to Size of Variables.c

--- Size of Variables.c
+++ Size of Variables.c
@@ -1,11 +1,10 @@
 #include<stdio.h>
 int main() {
+    printf("RA2211042010059") 
     int intType;
     float floatType;
     double doubleType;
     char characterType;
-
-
     printf("Size of int: %zu bytes\n", sizeof(intType));
     printf("Size of float: %zu bytes\n", sizeof(floatType));
     printf("Size of double: %zu bytes\n", sizeof(doubleType));
