5be277886d38f51dc9ebbccb828a4f6736207100
sankalpSRM <113873509+sankalpSRM@users.noreply.github.com>
2022-10-12 09:59:56 +0530

    Update and rename Palindrome checking to Palindrome checking.c

--- Palindrome checking.c
+++ Palindrome checking.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
-int main() {
-  int n, reversed = 0, remainder, original;
+int main() 
+{
+    printf("RA2211042010059")
+    int n, reversed = 0, remainder, original;
     printf("Enter an integer: ");
     scanf("%d", &n);
     original = n;
