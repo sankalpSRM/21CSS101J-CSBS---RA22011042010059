b5fa0a785866eb10e7c4fbf7a9217815b66aa40f
sankalpSRM <113873509+sankalpSRM@users.noreply.github.com>
2022-10-12 09:52:03 +0530

    Update and rename LCM to LCM.c

--- LCM.c
+++ LCM.c
@@ -1,14 +1,11 @@
 #include <stdio.h>
 
 int main() {
-
+    printf("RA2211042010059")
     int n1, n2, max;
-
     printf("Enter two positive integers: ");
     scanf("%d %d", &n1, &n2);
-
     max = (n1 > n2) ? n1 : n2;
-
     while (1) {
         if ((max % n1 == 0) && (max % n2 == 0)) {
             printf("The LCM of %d and %d is %d.", n1, n2, max);
