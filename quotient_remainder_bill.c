fa76db4f6007392629b3b849f47c4775f8a2ba59
sankalpSRM <113873509+sankalpSRM@users.noreply.github.com>
2022-11-03 23:00:46 +0530

    Create quotient_remainder_bill.c

---
+++ quotient_remainder_bill.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main () 
+{
+  printf("RA2211042010059);
+  int bill_amount,amount_given, q,r;
+  scanf("%d", &bill_amount);
+  scanf("%d", &amount_given):
+  q=bill_amount/amount_given;
+  r=bill_amount%amount_given;
+  printf("%d\n",q);
+  printf("%d\n",r);
+  
+  return 0;
+}
