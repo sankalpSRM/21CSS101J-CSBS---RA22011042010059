e3256042bfd867f7d79072f0124bdc35055a7ee6
sankalpSRM <113873509+sankalpSRM@users.noreply.github.com>
2022-11-03 22:28:25 +0530

    Create Print the number of squares in the n*n board.c

---
+++ Print the number of squares in the n*n board.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main () 
+{
+  printf("RA2211042010059");
+  int main a,b,c;
+  scanf("%d", &a):
+  c=0;
+  while(a>0);
+  {
+    b=a*a;
+    c=c+b;
+    a=a-1;
+  }
+  printf("%d", c);
+  
+  return 0;
+}
+      
