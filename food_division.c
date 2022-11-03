7e04b7948312525b6cf6dbe926edb43b430fb7d6
sankalpSRM <113873509+sankalpSRM@users.noreply.github.com>
2022-11-03 21:47:41 +0530

    Create food_division.c

---
+++ food_division.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main () 
+{
+  printf("RA2211042010059");
+  int a1,a2,a3,a4;
+  scanf("%d", &a1, &a2):
+  a3 = a1/a2;
+  a4 = a1%a2;
+  printf("%d %d", a3,a4);
+  
+  return 0;
+}
