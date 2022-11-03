58870d40cf7cc31542faaf4c9b887e4ec44e9d5f
sankalpSRM <113873509+sankalpSRM@users.noreply.github.com>
2022-11-03 21:36:25 +0530

    Update LCM.c

--- LCM.c
+++ LCM.c
@@ -1,17 +1,19 @@
 #include <stdio.h>
 
-int main() {
+int main() 
+{
     printf("RA2211042010059")
-    int n1, n2, max;
+     int n1, n2, i, gcd, lcm;
     printf("Enter two positive integers: ");
     scanf("%d %d", &n1, &n2);
-    max = (n1 > n2) ? n1 : n2;
-    while (1) {
-        if ((max % n1 == 0) && (max % n2 == 0)) {
-            printf("The LCM of %d and %d is %d.", n1, n2, max);
-            break;
-        }
-        ++max;
+    for (i = 1; i <= n1 && i <= n2; ++i) 
+    {
+        if (n1 % i == 0 && n2 % i == 0)
+            gcd = i;
     }
+
+    lcm = (n1 * n2) / gcd;
+
+    printf("The LCM of two numbers %d and %d is %d.", n1, n2, lcm);
     return 0;
 }
