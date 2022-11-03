9271081c1bf80d33261dcbf85f50874ed3f054a9
sankalpSRM <113873509+sankalpSRM@users.noreply.github.com>
2022-11-03 21:44:25 +0530

    Create Float_to_double.c

---
+++ Float_to_double.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main () 
+{
+  printf("RA2211042010059");
+  float v1, v2;
+  scanf("%f", &v1);
+  scanf("%f", &v2);
+  printf("%lf", v1);
+  printf("%lf", v2);
+  
+  return 0,
+}
+  
