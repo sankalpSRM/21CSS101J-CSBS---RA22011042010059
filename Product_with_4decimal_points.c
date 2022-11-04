ff1d9334e2a46f0811146cb4da00d7cbdff5b934
sankalpSRM <113873509+sankalpSRM@users.noreply.github.com>
2022-11-04 15:23:18 +0530

    Create Product_with_4decimal_points.c

---
+++ Product_with_4decimal_points.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main () 
+{
+  printf("RA2211042010059");
+  float a,b,product;
+  scanf("%f %f",&a , &b):
+  product=a*b;
+  printf("%.4f", product0;
+  return 0,
+         
+}
