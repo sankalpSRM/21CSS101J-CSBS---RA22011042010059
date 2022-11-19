526b0da0a7b51e7be0de99dd27b46c90d8771243
sankalpSRM <113873509+sankalpSRM@users.noreply.github.com>
2022-11-20 05:10:46 +0530

    Create SOE_POO.c

---
+++ SOE_POO.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    printf("RA2211042010059");
+    int num, evenSum=0, oddProd=1, rem, temp;
+    printf("Enter any number: ");
+    scanf("%d", &num);
+    while(num>0)
+    {
+        rem = num%10;
+        if(rem%2==0)
+            evenSum = evenSum + rem;
+        else
+            oddProd = oddProd * rem;
+        num = num/10;
+    }
+    printf("\nSum of Even Digit = %d", evenSum);
+    printf("\nProduct of Odd Digit = %d", oddProd);
+    getch();
+    return 0;
+}
