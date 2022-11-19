bc512864f6489a67acec089328d3a55f560c5468
sankalpSRM <113873509+sankalpSRM@users.noreply.github.com>
2022-11-20 04:35:06 +0530

    Create printf_string.c

---
+++ printf_string.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    printf("RA2211042010059");
+    char str[20];
+    printf("Enter your first name: ");
+    scanf("%s", str);
+    printf("Hello, %s", str);
+    getch();
+    return 0;
+}
