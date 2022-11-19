c50af203a47217df461c2e126709f8ac2b36dae1
sankalpSRM <113873509+sankalpSRM@users.noreply.github.com>
2022-11-20 05:12:10 +0530

    Create avg_pct.c

---
+++ avg_pct.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    printf("RA2211042010059");
+    int i;
+    float mark, sum=0, avg;
+    printf("Enter Marks obtained in 5 Subjects: ");
+    for(i=0; i<5; i++)
+    {
+        scanf("%f", &mark);
+        sum = sum+mark;
+    }
+    avg = sum/5;
+    printf("\nAverage Mark = %0.2f", avg);
+    getch();
+    return 0;
+}
