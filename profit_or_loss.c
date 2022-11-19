c6e784012626ca228d1428a04163e54f1e908dca
sankalpSRM <113873509+sankalpSRM@users.noreply.github.com>
2022-11-20 05:14:07 +0530

    Create profit_or_loss.c

---
+++ profit_or_loss.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    printf("RA2211042010059");
+    float sp, cp, pf, ls;
+    printf("Enter cost price: ");
+    scanf("%f", &cp);
+    printf("Enter selling price: ");
+    scanf("%f", &sp);
+    if(sp>cp)
+    {
+        pf = sp-cp;
+        printf("Profit = %.2f", pf);
+    }
+    else if(cp>sp)
+    {
+        ls = cp-sp;
+        printf("Loss = %.2f", ls);
+    }
+    else
+        printf("No profit and loss.");
+    getch();
+    return 0;
+}
