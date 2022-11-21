e88faae8bcdab83cf64ccbd07f6da38782f6dcdb
sankalpSRM <113873509+sankalpSRM@users.noreply.github.com>
2022-11-22 04:18:56 +0530

    Update decimal_octal_hexa.c

--- decimal_octal_hexa.c
+++ decimal_octal_hexa.c
@@ -3,8 +3,9 @@
 int main()
 {
     printf("RA2211042010059");
-    int value=2567;     
+    int value;   
     printf("Decimal value is: %d\n",value);
+    scanf("%d",&value);
     printf("Octal value is: %o\n",value);
     printf("Hexadecimal value is (Alphabet in small letters): %x\n",value);
     printf("Hexadecimal value is (Alphabet in capital letters): %X\n",value);
