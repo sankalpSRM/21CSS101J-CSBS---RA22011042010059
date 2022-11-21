166d5fbfb28856c73563de1eef10a6684d732d43
sankalpSRM <113873509+sankalpSRM@users.noreply.github.com>
2022-11-22 00:09:57 +0530

    Create Shoe_distribution.c

---
+++ Shoe_distribution.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("RA2211042010059");
+    int n,m;
+    printf("Number of friends: ");
+    scanf("%d\n", &n);
+    printf("Number of left shoes: ");
+    scanf("%d\n", &m);
+    printf("Extra pairs of shoes to be bought: %d\n", n-m);
+    printf("Right shoes to be bought: %d\n", m);
+}
