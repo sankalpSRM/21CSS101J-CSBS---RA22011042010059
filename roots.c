0fe5b03068621f454bcbe89289db12f124c4b656
sankalpSRM <113873509+sankalpSRM@users.noreply.github.com>
2022-11-24 22:57:57 +0530

    Update roots.c

--- roots.c
+++ roots.c
@@ -1,22 +1,35 @@
 #include <stdio.h>
+#include <math.h> 
 
 int main()
 {
-    int side1, side2, side3;
-    printf("Enter three sides of triangle: ");
-    scanf("%d%d%d", &side1, &side2, &side3);
+    float a, b, c;
+    float root1, root2, imaginary;
+    float discriminant;
+    
+    printf("Enter values of a, b, c of quadratic equation (aX^2 + bX + c): ");
+    scanf("%f%f%f", &a, &b, &c);
+    discriminant = (b * b) - (4 * a * c);
+    if(discriminant > 0)
+    {
+        root1 = (-b + sqrt(discriminant)) / (2*a);
+        root2 = (-b - sqrt(discriminant)) / (2*a);
 
-    if(side1==side2 && side2==side3) 
-    {        
-      printf("Equilateral triangle.");
+        printf("Two distinct and real roots exists: %.2f and %.2f", root1, root2);
     }
-    else if(side1==side2 || side1==side3 || side2==side3) 
+    else if(discriminant == 0)
     {
-       printf("Isosceles triangle.");
+        root1 = root2 = -b / (2 * a);
+
+        printf("Two equal and real roots exists: %.2f and %.2f", root1, root2);
     }
-    else 
+    else if(discriminant < 0)
     {
-        printf("Scalene triangle.");
+        root1 = root2 = -b / (2 * a);
+        imaginary = sqrt(-discriminant) / (2 * a);
+
+        printf("Two distinct complex roots exists: %.2f + i%.2f and %.2f - i%.2f", 
+                root1, imaginary, root2, imaginary);
     }
 
     return 0;
