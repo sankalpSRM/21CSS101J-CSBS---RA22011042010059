c70cac56c1de224beb5c6362ef22855d936d46a0
sankalpSRM <113873509+sankalpSRM@users.noreply.github.com>
2022-11-24 22:50:06 +0530

    Create triangle_angle_check.c

---
+++ triangle_angle_check.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("RA2211042010059");
+    int angle1, angle2, angle3, sum;
+    printf("Enter three angles of triangle: \n");
+    scanf("%d%d%d", &angle1, &angle2, &angle3);
+    sum = angle1 + angle2 + angle3; 
+    if(sum == 180 && angle1 > 0 && angle2 > 0 && angle3 > 0) 
+    {
+        printf("Triangle is valid.");
+    }
+    else
+    {
+        printf("Triangle is not valid.");
+    }
+
+    return 0;
+}
